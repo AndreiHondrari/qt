8adcf103c5517e68a130247158fd88a4511aec48
Hondrari Andrei-George <andrei.hondrari@gmail.com>
2015-12-23 10:56:15 +0200

    Partial implementation of MultiThreadedReading. Added a small/big file generator in python.

--- MultiThreadedReading/main.cpp
+++ MultiThreadedReading/main.cpp
@@ -3,19 +3,22 @@
 #include <QRunnable>
 #include <QFile>
 #include <QDebug>
-
+#include <QMutex>
 
 class FileTaskHandler : public QRunnable
 {
 public:
-    explicit FileTaskHandler(const QString &fileName, const QString &threadName);
-    virtual ~FileTaskHandler();
+    explicit FileTaskHandler(QFile * input_file, QFile * output_file, QMutex * mutex);
 
     void run();
 
 private:
-    QFile *m_file;
-    QString m_name;
+    void message(QString msg);
+
+private:
+    QFile *m_input_file;
+    QFile *m_output_file;
+    QMutex *m_mutex;
 };
 
 int main(int argc, char *argv[])
@@ -24,27 +27,69 @@ int main(int argc, char *argv[])
 
     QList<FileTaskHandler*> fileTasks;
 
+    QMutex mutex;
+    QFile input_file("somebigfile.txt");
+    QFile output_file("someoutputfile.txt");
+
+    input_file.open(QFile::ReadOnly);
+    output_file.open(QFile::WriteOnly);
+
     for(ushort i = 0; i < 4; ++i) {
-        FileTaskHandler *fileTaskhandler = new FileTaskHandler("somefile.txt", QString("{0}{1}").arg("THREAD #", i));
+        FileTaskHandler *fileTaskhandler = new FileTaskHandler(
+            &input_file,
+            &output_file,
+            &mutex
+        );
         fileTasks.append(fileTaskhandler);
         QThreadPool::globalInstance()->start(fileTaskhandler);
     }
 
+    QThreadPool::globalInstance()->waitForDone();
+    input_file.close();
+    output_file.close();
+    qDebug() << "FINISHED !";
+
     return a.exec();
 }
 
-FileTaskHandler::FileTaskHandler(const QString &fileName, const QString &threadName) :
-    m_file(new QFile(fileName)),
-    m_name(threadName)
+FileTaskHandler::FileTaskHandler(QFile * input_file, QFile * output_file, QMutex * mutex) :
+    m_input_file(input_file),
+    m_output_file(output_file),
+    m_mutex(mutex)
 {
 
-}
-
-FileTaskHandler::~FileTaskHandler()
-{
-    delete m_file;
 }
 
 void FileTaskHandler::run() {
-    qDebug() << m_name;
+
+    message("Started!");
+
+    if(!m_input_file->exists()) {
+        message(": File " + m_input_file->fileName() + " does not exist");
+        return;
+    }
+
+    if(!m_output_file->exists()) {
+        message(": File " + m_output_file->fileName() + " does not exist");
+        return;
+    }
+
+    if(!m_input_file->isOpen() || m_input_file->openMode() != QFile::ReadOnly) {
+        message(": File " + m_input_file->fileName() + " is not opened in read-only mode");
+        return;
+    }
+
+    if(!m_output_file->isOpen() || m_output_file->openMode() != QFile::WriteOnly) {
+        message(": File " + m_output_file->fileName() + " is not opened in write-only mode");
+        return;
+    }
+
+    message("Ended !");
+}
+
+void FileTaskHandler::message(QString msg)
+{
+    m_mutex->lock();
+    qDebug().noquote() << QStringLiteral("THREAD ") << QString("%1").arg((quintptr)QThread::currentThreadId()).rightJustified(4) << ": " << msg;
+    m_mutex->unlock();
 }
---
+++ genbigfile.py
@@ -0,0 +1,11 @@
+
+
+myfile = open("somebigfile.txt", "w")
+
+BIG = 20000000
+SMALL = 50000
+
+for i in range(SMALL):
+	myfile.write("wafafawhguaghwuaghuwahufhauwh y3478258171089727c214891287x4217x89489217c894b2 74289 41876982")
+
+myfile.close()
